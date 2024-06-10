cf533845b428502573ffb848173692a73d4c4e2c
Yashar <yashar22mo@gmail.com>
2024-06-10 22:24:43 +0330

    Add a new component and added to this project

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import logo from './logo.svg';
+import Name from './Name';
 import './App.css';
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <Name></Name>
         <a
           className="App-link"
           href="https://reactjs.org"
---
+++ src/Name.js
@@ -0,0 +1,7 @@
+function Name () {
+    return (
+        <h1>Yashar Mohammadi</h1>
+    )
+}
+
+export default Name
