0ed0bad7450743905a36d63eb4265d42650299b5
Yashar <yashar22mo@gmail.com>
2024-06-20 15:47:45 +0330

    Work with child props

--- src/Main/Main.js
+++ src/Main/Main.js
@@ -1,11 +1,18 @@
 import Users from '../Users/Users'
 
 export default function Main () {
+    let users = [
+        {id: 1, name: 'Yashar',},
+        {id: 2, name: 'Mojtaba',},
+        {id: 3, name: 'Arshia',}
+    ]
     return (
         <div>
-            <Users color="red"/>
-            <Users color="blue"/>
-            <Users color="pink"/>
+            <Users {...users[0]}/>
+            <Users {...users[1]}>
+                <span>(Child)</span>
+            </Users>
+            <Users {...users[2]}/>
         </div>
     )
 }
--- src/Users/Users.js
+++ src/Users/Users.js
@@ -1,9 +1,17 @@
-import React from "react"
+import React from "react";
 
-export default function Users (props) {
-    return (
-        <div>
-            <h1>{props.color}</h1>
-        </div>
-    )
+// export default function Users ({id, name: color}) {   // set new name for props name
+// export default function Users(id = 3, name = 'Color') {  // default value for props
+export default function Users(props) {
+    // let {id, name} = props; // store props value in this obj
+	return (
+		<div>
+			<h1>{props.id}. {props.name} {props.children}</h1>
+		</div>
+	);
 }
+
+// Users.defaultProps = { // default value for props
+//     id: 3,
+//     name: 'Color'
+// }
