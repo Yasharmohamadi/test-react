e3210e6ebb3e94f906ae66879e5ecdc420ad3011
Yashar <yashar22mo@gmail.com>
2024-06-20 16:19:31 +0330

    Work with props in class component

--- src/Users/Users.js
+++ src/Users/Users.js
@@ -2,13 +2,29 @@ import React from "react";
 
 // export default function Users ({id, name: color}) {   // set new name for props name
 // export default function Users(id = 3, name = 'Color') {  // default value for props
-export default function Users(props) {
-    // let {id, name} = props; // store props value in this obj
-	return (
-		<div>
-			<h1>{props.id}. {props.name} {props.children}</h1>
-		</div>
-	);
+
+// create  component with function
+
+// export default function Users(props) {
+//     // let {id, name} = props; // store props value in this obj
+// 	return (
+// 		<div>
+// 			<h1>{props.id}. {props.name} {props.children}</h1>
+// 		</div>
+// 	);
+// }
+
+// create  component with class
+export default class Users extends React.Component {
+	render() {
+		return (
+			<div>
+				<h1>
+					{this.props.id}. {this.props.name} {this.props.children}
+				</h1>
+			</div>
+		);
+	}
 }
 
 // Users.defaultProps = { // default value for props
