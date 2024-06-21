e2e64c221b827ee896cae0100c4bcf5f54b1fc49
Yashar <yashar22mo@gmail.com>
2024-06-21 13:13:45 +0330

    Work with state in class components

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,18 +1,43 @@
-import Users from '../Users/Users'
+import Users from "../Users/Users";
+import React from "react";
 
-export default function Main () {
-    let users = [
-        {id: 1, name: 'Jack',},
-        {id: 2, name: 'John',},
-        {id: 3, name: '',}
-    ]
-    return (
-        <div>
-            <Users {...users[0]}/>
-            <Users {...users[1]}>
-                <span>(Child)</span>
-            </Users>
-            <Users/>
-        </div>
-    )
+// export default function Main () {
+// let users = [
+// {id: 1, name: 'Jack',},
+// {id: 2, name: 'John',},
+// {id: 3, name: 'John',}
+// ]
+//     return (
+//         <div>
+//             <Users {...users[0]}/>
+//             <Users id={2} name={5}>
+//                 <span>(Child)</span>
+//             </Users>
+//             {/* <Users/> */}
+//         </div>
+//     )
+// }
+
+// work with class and state
+export default class Main extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			users: [
+				{ id: 1, name: "Jack" },
+				{ id: 2, name: "John" },
+				{ id: 3, name: "John" },
+			],
+		};
+	}
+
+	render() {
+		return (
+			<div>
+				<Users {...this.state.users[0]} />
+				<Users {...this.state.users[1]} />
+				<Users {...this.state.users[2]} />
+			</div>
+		);
+	}
 }
--- src/components/Users/Users.js
+++ src/components/Users/Users.js
@@ -1,56 +1,55 @@
 import React from "react";
 
-
 /*  
 ======== FUNCTIONAL COMPONENTS ========
-*/  
-
+*/
 
 // create  component with function
 
-
 // set new name for props name in functional component
-// export default function Users ({id, name: username}) {   
+// export default function Users ({id, name: username}) {
 
 // set default value for props in functional component
-// export default function Users(id = 3, name = 'User') {  
+// export default function Users(id = 3, name = 'User') {
 
-// export default function Users(props) {
-//     // let {id, name} = props; // store props value in this obj
-// 	return (
-// 		<div>
-// 			<h1>{props.id}. {props.name} {props.children}</h1>
-// 		</div>
-// 	);
+export default function Users(props) {
+    // let {id, name} = props; // store props value in this obj
+	return (
+		<div>
+			<h1>{props.id}. {props.name} {props.children}</h1>
+		</div>
+	);
+}
+
+// set default value for props in functional component
+Users.defaultProps = {
+    id: '?',
+    name: 'User'
+}
+
+/*  
+========== CLASS COMPONENTS ===========
+*/
+
+// // create  component with class
+// export default class Users extends React.Component {
+// 	render(name = "User") {
+// 		return (
+// 			<div>
+// 				<h1>
+// 					{this.props.id}. {this.props.name} {this.props.children}
+// 				</h1>
+// 			</div>
+// 		);
+// 	}
 // }
 
-// set default value for props in functional component
-// Users.defaultProps = { 
+// set default value for props in class component
+// Users.defaultProps = {
 //     id: 3,
 //     name: 'User'
 // }
 
-
-
-/*  
-========== CLASS COMPONENTS ===========
-*/  
-
-// create  component with class
-export default class Users extends React.Component {
-	render(name = 'User') {
-		return (
-			<div>
-				<h1>
-					{this.props.id}. {this.props.name} {this.props.children}
-				</h1>
-			</div>		
-		);
-	}	
-}
-
-// set default value for props in class component
-Users.defaultProps = { 
-    id: 3,
-    name: 'User'
-}
+// Users.propTypes = {
+// 	name: propTypes.string
+// }
