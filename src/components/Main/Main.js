2b31422e16b0fd23a71fa3194bcd56c2d2878a71
Yashar <yashar22mo@gmail.com>
2024-06-21 15:26:57 +0330

    Work with event in react

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -22,18 +22,36 @@ import React from "react";
 export default class Main extends React.Component {
 	constructor(props) {
 		super(props);
+
 		this.state = {
 			users: [
-				{ id: 1, name: "Jack" },
-				{ id: 2, name: "John" },
-				{ id: 3, name: "John" },
+				{ id: 1, name: "Adolf" },
+				{ id: 2, name: "Joseph" },
+				{ id: 3, name: "Isaac" },
 			],
 		};
+
+		// setTimeout(() => {
+		//     this.setState({
+		//         users: [
+		//             { id: 1, name: "Tom" },
+		//             { id: 2, name: "Donald" },
+		//             { id: 3, name: "Winston" },
+		//         ],
+		//     });
+		// }, 1500);
+	}
+
+	clickHandler() {
+		alert("Clicked !");
 	}
 
 	render() {
 		return (
 			<div>
+				<button onClick={this.clickHandler}>
+                    <h2>Click Here</h2>
+                </button>
 				<Users {...this.state.users[0]} />
 				<Users {...this.state.users[1]} />
 				<Users {...this.state.users[2]} />
