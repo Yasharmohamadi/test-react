0b3f0b8d77d5fafa7599a828190b98873b790e77
Yashar <yashar22mo@gmail.com>
2024-06-23 20:12:11 +0330

    Use create root insted reactDOM

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import './index.css';
 import Main from './components/Main/Main'
 
 
 // FIRST WAY TO USE REACT
-ReactDOM.render(
-    <Main />,
-    document.getElementById('root')
-);
+// ReactDOM.render(
+//     <Main />,
+//     document.getElementById('root')
+// );
 
 // SECOND WAY TO USE REACT
-// const root = ReactDOM.createRoot(document.getElementById('root'));
-// root.render(
-//     <Main />
-// );
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+    <Main />
+);
