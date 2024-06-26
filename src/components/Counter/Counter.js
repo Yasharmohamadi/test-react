62b823b9b4cab06864e6556651b50a4630ef192b
Yashar <yashar22mo@gmail.com>
2024-06-26 20:40:07 +0330

    add inline css style for counter button

--- src/components/Counter/Counter.js
+++ src/components/Counter/Counter.js
@@ -1,13 +1,13 @@
 export default function Counter({count, add, sub}) {
 	return (
 		<div>
-			<button onClick={sub}>
+			<button onClick={sub} style={{background: 'Red'}}>
 				<h2>-</h2>
 			</button>
 			<button>
 				<h2>{count}</h2>
 			</button>
-			<button onClick={add}>
+			<button onClick={add} style={{background: 'Green'}}>
 				<h2>+</h2>
 			</button>
 		</div>
