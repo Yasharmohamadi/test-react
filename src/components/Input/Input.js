696db65475e4f7b230d22ed3647952e5aa5901de
Yashar <yashar22mo@gmail.com>
2024-06-26 20:36:43 +0330

    add inline css style for input

--- src/components/Input/Input.js
+++ src/components/Input/Input.js
@@ -1,7 +1,7 @@
 export default function Input ({value, onChange}) {
     return (
         <div>
-            <input maxLength={10} placeholder="Type anything here ..." onChange={onChange}/>
+            <input style={{padding: 10, borderRadius: 5, fontSize: 20}} maxLength={10} placeholder="Type anything here ..." onChange={onChange}/>
             <h3>{value}</h3>
         </div>
     )
