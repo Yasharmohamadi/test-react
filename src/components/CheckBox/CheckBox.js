0d7ec67fc5432419920b5fd57c776840ff8506d8
Yashar <yashar22mo@gmail.com>
2024-07-25 11:16:56 +0330

    Delete all component

--- src/components/CheckBox/CheckBox.js
+++
@@ -1,8 +0,0 @@
-export default function CheckBox ({checked, onChange}) {
-    return (
-        <div>
-            <label style={{fontSize: 20}} htmlFor="checkbox">Check Box</label>
-            <input checked={checked} type="checkbox" id="checkbox" onChange={onChange} />
-        </div>
-    )
-}
--- src/components/Input/Input.js
+++
@@ -1,8 +0,0 @@
-export default function Input ({value, onChange}) {
-    return (
-        <div>
-            <input style={{padding: 10, borderRadius: 5, fontSize: 20}} maxLength={10} placeholder="Type anything here ..." onChange={onChange}/>
-            <h3>{value}</h3>
-        </div>
-    )
-}
