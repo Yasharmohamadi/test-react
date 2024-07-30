06295e9e5d1cc15af02212094e706e56668b41c3
Yashar <yashar22mo@gmail.com>
2024-07-30 17:37:07 +0330

    Working with DB

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,20 +1,55 @@
 import React from "react";
-import { Modal } from "react-bootstrap";
+import { Table, Container, Alert } from "react-bootstrap";
 
 export default class Main extends React.Component {
 	constructor(props) {
 		super(props);
 
 		this.state = {
-			showModal: a,
+			posts: [],
 		};
+	}
 
+	componentDidMount() {
+		fetch("https://jsonplaceholder.typicode.com/users", {
+			method: "GET",
+		})
+			.then((response) => response.json())
+			.then((data) => {
+				this.setState({
+					posts: data,
+				});
+			});
 	}
 
 	render() {
 		return (
-			<div>
-			</div>
+			<Container className="mt-5">
+				{this.state.posts.length > 0 ? (
+					<Table striped bordered hover>
+						<thead>
+							<tr>
+								<th>ID</th>
+								<th>Name</th>
+								<th>Email</th>
+								<th>Phone</th>
+							</tr>
+						</thead>
+						{this.state.posts.map((post) => (
+							<tbody>
+								<tr>
+									<td>{post.id}</td>
+									<td>{post.name}</td>
+									<td>{post.email}</td>
+									<td>{post.phone}</td>
+								</tr>
+							</tbody>
+						))}
+					</Table>
+				) : (
+					<Alert variant="danger">Your database is not correct !</Alert>
+				)}
+			</Container>
 		);
 	}
 }
