942f8884a0557f63c8f6b8db67a8be5b164fcdb1
Yashar <yashar22mo@gmail.com>
2024-07-31 11:09:08 +0330

    Install @mui/icons-material

--- src/components/Material/Material.js
+++ src/components/Material/Material.js
@@ -1,31 +1,30 @@
-import Alert from "@mui/material/Alert";
+import { Alert } from "@mui/material";
+import { PersonIcon } from '@mui/icons-material'
 import { Container } from "react-bootstrap";
 
 export default function BasicButtons() {
 	return (
 		<Container>
+			<hr />
 			<Alert severity="success">This is a success Alert.</Alert>
 			<br />
 			<Alert variant="filled" severity="success">
 				This is a filled success Alert.
 			</Alert>
-			<br />
-
-			<br />
+			<hr />
 			<Alert severity="info">This is an info Alert.</Alert>
 			<br />
 			<Alert variant="filled" severity="info">
 				This is a filled info Alert.
 			</Alert>
-			<br />
+			<hr />
 			<Alert severity="warning">This is a warning Alert.</Alert>
 			<br />
 			<Alert variant="filled" severity="warning">
 				This is a filled warning Alert.
 			</Alert>
-			<br />
+			<hr />
 			<Alert severity="error">This is an error Alert.</Alert>
-
 			<br />
 			<Alert variant="filled" severity="error">
 				This is a filled error Alert.
