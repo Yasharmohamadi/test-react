1ba6baf6a1bba1333cfb238f436c156ab54b25e2
Yashar <yashar22mo@gmail.com>
2024-08-02 09:57:04 +0330

    Handle signin POST

--- src/components/Panel/Login.js
+++ src/components/Panel/Login.js
@@ -1,31 +1,44 @@
 import React from "react";
-import { Typography } from "@mui/material";
-import { Form, InputGroup, Button } from "react-bootstrap";
 
 export default function Login() {
 	return (
 		<div>
-			<InputGroup className="mb-3 mt-3">
-				<Typography variant="h2" className="mt-4">
-					{" "}
-					Login
-				</Typography>
-			</InputGroup>
+			<form>
+				<h1>Login</h1>
+				<input
+					placeholder="Username"
+					style={{
+						width: "95%",
+						border: "1px solid #ccc",
+						margin: "10px",
+						padding: "10px",
+						borderRadius: "5px",
+					}}
+				/>
 
-			<InputGroup className="mb-3 mt-3">
-				<InputGroup.Text id="basic-addon1">@</InputGroup.Text>
-				<Form.Control placeholder="Username" />
-			</InputGroup>
-
-			<InputGroup className="mb-3">
-				<Form.Control placeholder="Enter Your Password ..." />
-			</InputGroup>
-
-			<div className="d-grid gap-2">
-				<Button variant="secondary" size="lg">
-					Login
-				</Button>
-			</div>
+				<input
+					placeholder="Password ..."
+					style={{
+						width: "95%",
+						border: "1px solid #ccc",
+						margin: "10px",
+						padding: "10px",
+						borderRadius: "5px",
+					}}
+				/>
+				<input
+					placeholder="Password ..."
+					type="submit"
+					style={{
+						width: "95%",
+						border: "1px solid #ccc",
+						margin: "10px",
+						padding: "10px",
+						borderRadius: "5px",
+					}}
+					value="Login"
+				/>
+			</form>
 		</div>
 	);
 }
--- src/components/Panel/Signin.js
+++ src/components/Panel/Signin.js
@@ -1,36 +1,76 @@
-import React from "react";
-import { Typography } from "@mui/material";
-import { Form, InputGroup, Button } from "react-bootstrap";
+import React, { useState } from "react";
 
 export default function Signin() {
+	const [username, setUsername] = useState("");
+	const [email, setEmail] = useState("");
+	const [password, setPassword] = useState("");
+
+	const registerHandler = (event) => {
+		event.preventDefault();
+
+		let newUser = [username, email, password];
+
+		fetch("https://react-app-e0af5-default-rtdb.firebaseio.com/users.json", {
+			method: "POST",
+			body: JSON.stringify(newUser),
+		})
+			.then((response) => console.log(response))
+			.fetch((error) => console.log(error));
+	};
+
 	return (
 		<div>
-			<InputGroup className="mb-3 mt-3">
-				<Typography variant="h2" className="mt-4">
-					{" "}
-					Signin
-				</Typography>
-			</InputGroup>
-
-			<InputGroup className="mb-3 mt-3">
-				<InputGroup.Text id="basic-addon1">@</InputGroup.Text>
-				<Form.Control placeholder="Username" />
-			</InputGroup>
-
-			<InputGroup className="mb-3">
-				<Form.Control placeholder="Enter Your Email ..." />
-				<InputGroup.Text id="basic-addon2">@gmail.com</InputGroup.Text>
-			</InputGroup>
-
-			<InputGroup className="mb-3">
-				<Form.Control placeholder="Enter Your Password ..." />
-			</InputGroup>
-
-			<div className="d-grid gap-2">
-				<Button variant="secondary" size="lg">
-        Signin
-				</Button>
-			</div>
+			<form onSubmit={registerHandler}>
+				<h1> Signin</h1>
+				<input
+					placeholder="Username"
+					value={username}
+					onChange={(event) => setUsername(event.target.value)}
+					style={{
+						width: "95%",
+						border: "1px solid #ccc",
+						margin: "10px",
+						padding: "10px",
+						borderRadius: "5px",
+					}}
+				/>
+				<input
+					placeholder="Email ..."
+					value={email}
+					onChange={(event) => setEmail(event.target.value)}
+					style={{
+						width: "95%",
+						border: "1px solid #ccc",
+						margin: "10px",
+						padding: "10px",
+						borderRadius: "5px",
+					}}
+				/>
+				<input
+					placeholder="Password ..."
+					value={password}
+					onChange={(event) => setPassword(event.target.value)}
+					style={{
+						width: "95%",
+						border: "1px solid #ccc",
+						margin: "10px",
+						padding: "10px",
+						borderRadius: "5px",
+					}}
+				/>
+				<input
+					placeholder="Password ..."
+					type="submit"
+					style={{
+						width: "95%",
+						border: "1px solid #ccc",
+						margin: "10px",
+						padding: "10px",
+						borderRadius: "5px",
+					}}
+					value="Signin"
+				/>
+			</form>
 		</div>
 	);
 }
