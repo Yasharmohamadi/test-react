8b13410903d0df8a76d96c337d216d6f8420c2ab
Yashar <yashar22mo@gmail.com>
2024-08-31 22:04:20 +0330

    use react-toastify

--- src/Pages/Alerts/Alerts.js
+++ src/Pages/Alerts/Alerts.js
@@ -6,13 +6,33 @@ import { Button } from "@mui/material";
 export default function Alerts() {
 	return (
 		<div>
-			<Button
-				color="error"
-				variant="contained"
-				onClick={() => toast("hi, I'm Alert !")}
-			>
-				Click to show Alert !
+
+			<Button color="error" variant="outlined" onClick={() => toast.error("")}>
+				toast error
 			</Button>
+			<br />
+			<Button
+				color="success"
+				variant="outlined"
+				onClick={() => toast.success("")}
+			>
+				toast success
+			</Button>
+			<br />
+			<Button color="info" variant="outlined" onClick={() => toast.info("")}>
+				toast info
+			</Button>
+			<br />
+			<Button
+				color="warning"
+				variant="outlined"
+				onClick={() => toast.warning("")}
+			>
+				toast warning
+			</Button>
+			<br />
+
+
 			<ToastContainer />
 		</div>
 	);
