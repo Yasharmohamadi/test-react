6bf73ce3036941ca8a73881e7a1fb63a12eea8dc
Yashar <yashar22mo@gmail.com>
2024-08-31 21:41:02 +0330

    use react-toastify in alerts page

--- src/Pages/Alerts/Alerts.js
+++ src/Pages/Alerts/Alerts.js
@@ -1,7 +1,19 @@
-import React from 'react'
+import React from "react";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { Button } from "@mui/material";
 
 export default function Alerts() {
-  return (
-    <div>Alerts</div>
-  )
+	return (
+		<div>
+			<Button
+				color="error"
+				variant="contained"
+				onClick={() => toast("hi, I'm Alert !")}
+			>
+				Click to show Alert !
+			</Button>
+			<ToastContainer />
+		</div>
+	);
 }
